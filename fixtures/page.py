2d333fff42c2f31b5bf33ed787ca7362fd765900
ivan <it000309@gmail.com>
2025-06-26 16:04:30 +0300

    Jenkinsfile updated

--- fixtures/page.py
+++ fixtures/page.py
@@ -5,7 +5,7 @@ from utils.logger import logger
 
 def pytest_addoption(parser):
     parser.addoption('--browser', default='chrome', help="Choose browser: chrome or firefox")
-    parser.addoption('--h', default=False, help='Choose headless: True or False')
+    parser.addoption('--h', default=True, help='Choose headless: True or False')
     parser.addoption('--slow', default=200, help='Choose slow_mo for robot action')
 
 @pytest.fixture()
